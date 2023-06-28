c1799abf3748b4dfcde08909366302b6f458b138
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-06-28 14:32:04 +0100

    add: mema_content_frame.py

--- Codebank/mema_main_window.py
+++ Codebank/mema_main_window.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from mema_constants import *
 from mema_button_frame import *
-from mema_frame import *
+from mema_content_frame import *
 
 class main_window(Tk):
 
@@ -11,23 +11,24 @@ class main_window(Tk):
         Tk.__init__(self, *args, **kwargs)
         self.configure_tk_instance()
 
-        # Add in frame
+        # Mainloop
+        self.mainloop()
+    
+    def configure_tk_instance(self) -> None:
+    
+        # Sets the window geometry to the mema3 physical screen
+        self.geometry(MEMA_SCREEN_DIMENSIONS)
+
+        # Configures the row and column weightings
         self.grid_rowconfigure(0, weight = 1)
         self.grid_columnconfigure(0, weight = 3)
         self.grid_columnconfigure(1, weight = 1)
+
 
         self.content_frame: content_frame = content_frame(self)
         self.content_frame.grid(row=0, column=0, sticky=NSEW)
 
         self.button_frame: button_frame = button_frame(self)
         self.button_frame.grid(row=0,column=1, sticky=NSEW)
-
-        # Mainloop
-        self.mainloop()
-    
-    def configure_tk_instance(self) -> None:
-        
-        # Sets the window geometry to the mema3 physical screen
-        self.geometry(MEMA_SCREEN_DIMENSIONS)
 
 x = main_window()
