1ef1096e97b92147c61abfbd6350c022ac1852f8
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-06-28 15:34:12 +0100

    add: multithreading to playsound

--- Codebank/mema_speech.py
+++ Codebank/mema_speech.py
@@ -1,15 +1,32 @@
 from gtts import gTTS
-import os
-import playsound
-import threading
+from os import remove
+from playsound import playsound
+from threading import *
+from time import time
+ 
+def speak_thread(text) -> None:
 
-def speak_thread(text):
-    tts = gTTS(text=text, lang='en')
+    # Create a Google TTS Object
+    tts: gTTS = gTTS(text=text, lang='en')
 
-    filename = "abc.mp3"
-    tts.save(filename)
-    playsound.playsound(filename)
-    os.remove(filename)
+    # Generating a pseudorandom file name using the current time in milliseconds
+    # Since we need to save the file to play it, this can prevent duplicate file name errors.
+    # If on the odd chance that this fails, it will be caught by the try-except
+    filename: str = str(time() * 100000).replace('.','s') + ".mp3"
+
+    # Save the file, play is using the playsound library then remove it.
+    try:
+        tts.save(filename)
+        playsound(filename)
+        remove(filename)
+
+    except Exception as e:
+        print(f"speak_thread: error playing text, exception: {e}")
+
+    return None
 
 def speak(text):
-    threading.Thread(target=speak_thread, args=(text,), daemon=True).start()
+    
+    # This has to be run asynchronously on it's own thread.
+    Thread(target=speak_thread, args=(text,), daemon=True).start()
+    return None
