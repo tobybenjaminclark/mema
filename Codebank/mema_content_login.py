8ee21b3ecafb1eb3b9332a330f26fe802fe99729
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-06-28 15:12:03 +0100

    add: tts support with gTTS into mema_speech.py

--- Codebank/mema_content_login.py
+++ Codebank/mema_content_login.py
@@ -5,3 +5,7 @@ class content_login(content_frame):
 
     def __init__(self, *args, **kwargs)-> None:
         content_frame.__init__(self, *args, **kwargs)
+
+        self.label = Label(self, text = "LOGIN")
+        self.label.grid(row=0, column=0)
+        
