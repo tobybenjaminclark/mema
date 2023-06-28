70a59aaf2284fff8689f70c53ad3be22c005a444
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-06-28 15:43:44 +0100

    add: webcam to login screen

--- Codebank/mema_content_login.py
+++ Codebank/mema_content_login.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from mema_content_frame import *
+from PIL import Image, ImageTk
+import cv2
 
 class content_login(content_frame):
 
@@ -8,4 +10,22 @@ class content_login(content_frame):
 
         self.label = Label(self, text = "LOGIN")
         self.label.grid(row=0, column=0)
-        
+        self.label =Label(self)
+        self.label.grid(row=0, column=0)
+        self.cap= cv2.VideoCapture(0)
+        self.show_frames()
+
+    # Define function to show frame
+    def show_frames(self):
+        # Get the latest frame and convert into Image
+        cv2image= cv2.cvtColor(self.cap.read()[1],cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(cv2image)
+        # Convert image to PhotoImage
+        imgtk = ImageTk.PhotoImage(image = img)
+        self.label.imgtk = imgtk
+        self.label.configure(image=imgtk)
+        # Repeat after an interval to capture continiously
+        self.label.after(20, self.show_frames)
+
+
+
