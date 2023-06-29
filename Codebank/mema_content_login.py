e33fffffd457c34bd2f82ce23858d35e8dff57ca
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-06-29 16:00:45 +0100

    add: documentation to mema_facial_recognition.py

--- Codebank/mema_content_login.py
+++ Codebank/mema_content_login.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from mema_content_frame import *
+from mema_facial_recognition import *
 from PIL import Image, ImageTk
 import cv2
 
@@ -9,26 +10,8 @@ class content_login(content_frame):
         content_frame.__init__(self, *args, **kwargs)
 
         # Setup webcam frame
-        self.label: Label
-        self.label = Label(self, width = 800, height = 500)
-        self.label.grid(row=0, column=0)
-
-        # Using open-cv2 video capture
-        self.cap: cv2.VideoCapture = cv2.VideoCapture("/dev/video0")
-        self.show_frames()
-
-    def show_frames(self) -> None:
-
-        # Grabs the latest frame from the video capture & saves as image
-        img: Image = Image.fromarray(cv2.cvtColor(self.cap.read()[1],cv2.COLOR_BGR2RGB))
-
-        # Convert image frame to PhotoImage (compatiable with Tkinter)
-        imgtk = ImageTk.PhotoImage(image = img)
-        self.label.imgtk = imgtk
-        self.label.configure(image=imgtk)
-
-        # Repeat after an interval to capture continiously
-        self.label.after(20, self.show_frames)
+        facial_recognition_frame = facial_recognition(self)
+        facial_recognition_frame.pack()
 
 
 
--- Codebank/mema_facial_recognition.py
+++ Codebank/mema_facial_recognition.py
@@ -32,6 +32,7 @@ class facial_recognition(Frame):
         self.display_recognition()
 
     def load_face_encodings(self) -> None:
+        
         # Load a sample picture and learn how to recognize it.
         obama_image: face_recognition.ndarray = face_recognition.load_image_file("obama.jpg")
         obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
@@ -52,15 +53,38 @@ class facial_recognition(Frame):
         ]
 
     def display_recognition(self) -> None:
+
+        # Grabs the current frame from the webcam and attempts to recognize faces within the frame image. Returns a 
+        frame = self.process_frame()
+
+        # Manipulates the frame image, adding nametags for the detected faces, this will be displayed if FACIAL_RECOGNITION_DEBUG_MODE is True
+        self.render_nametags(frame)
+        
+        # Displays the updated frame in the Tk Frame instance and schedules another scan
+        self.display_and_reschedule(frame)
+
+    def process_frame(self) -> Image:
+        """
+        Process a single frame of video, returns the processed frame
+        """
+        
         # Grab a single frame of video
+        ret: cv2.OutputArray
+        frame: Image
         ret, frame = self.video_capture.read()
+
+        # Not needed, so removed
+        del ret
 
         # Only process every other frame of video to save time
         if self.process_this_frame:
+
             # Resize frame of video to 1/4 size for faster face recognition processing
+            small_frame: cv2.Mat
             small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
             # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame: cv2.Mat
             rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
             
             # Find all the faces and face encodings in the current frame of video
@@ -68,31 +92,46 @@ class facial_recognition(Frame):
             self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
 
             self.face_names = []
-            for face_encoding in self.face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(self.known_faces, face_encoding)
-                name = "Unknown"
+            self.match_known_faces()
 
-                # # If a match was found in self.known_faces, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = self.known_names[first_match_index]
-
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(self.known_faces, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = self.known_names[best_match_index]
-
-                self.face_names.append(name)
-
+        # Toggle the flag for processing the next frame
         self.process_this_frame = not self.process_this_frame
+        
+        # Return the processed frame
+        return frame
 
-        self.render_nametags(frame)
-                
-                
-        self.display_and_reschedule(frame)
-            
+    def match_known_faces(self) -> None:
+        """
+        Matches the face encodings with the known faces and assigns names to the recognized faces.
+        """
+
+        # List to store the recognized names
+        self.face_names: list[str] = [] 
+
+        for face_encoding in self.face_encodings:
+
+            # Compare the face encoding with the known faces to check for matches
+            matches: list[any]
+            matches = face_recognition.compare_faces(self.known_faces, face_encoding)
+
+            # Default name if no match is found
+            name: str = "Unknown"
+
+            # Find the known face with the smallest distance to the new face
+            # The smallest distance indicates the best match
+            face_distances: (cv2.ndarray | any)
+            face_distances = face_recognition.face_distance(self.known_faces, face_encoding)
+
+            # Uses numpy to find the closest match
+            best_match_index: np.intp
+            best_match_index = np.argmin(face_distances)
+
+            # If a match is found in known_faces, assign the corresponding name to the recognized face
+            if matches[best_match_index]: name = self.known_names[best_match_index]
+
+            # Append the recognized name to the list of face names
+            self.face_names.append(name)
+
     def render_nametags(self, frame) -> None:
         # Display the results
         for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
@@ -118,19 +157,8 @@ class facial_recognition(Frame):
         # Schedule the next frame update
         self.after(10, self.display_recognition)
 
-
     def quit(self) -> None:
 
         # Release handle to the webcam
         self.video_capture.release()
         cv2.destroyAllWindows()
-
-# Create the main Tkinter window
-root = Tk()
-
-# Create the facial recognition frame
-facial_recognition_frame = facial_recognition(root)
-facial_recognition_frame.pack()
-
-# Start the Tkinter event loop
-root.mainloop()
