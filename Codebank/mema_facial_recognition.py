cf113c98fdb33a5c6bf6aebe985eaa791af26772
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-06-30 10:24:07 +0100

    add: speech recognition base

--- Codebank/mema_facial_recognition.py
+++ Codebank/mema_facial_recognition.py
@@ -38,11 +38,11 @@ class facial_recognition(Frame):
     def load_face_encodings(self) -> None:
         
         # Load a sample picture and learn how to recognize it.
-        obama_image: face_recognition.ndarray = face_recognition.load_image_file("obama.jpg")
+        obama_image: face_recognition.ndarray = face_recognition.load_image_file("Databank/obama.jpg")
         obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
         # Load a second sample picture and learn how to recognize it.
-        biden_image: face_recognition.ndarray = face_recognition.load_image_file("biden.jpeg")
+        biden_image: face_recognition.ndarray = face_recognition.load_image_file("Databank/biden.jpeg")
         biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
         # Create arrays of known face encodings and their names
