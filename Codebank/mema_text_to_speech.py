17ef79fb38cafbd1d3f4231a264e5f4bc0186fe7
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-06-30 15:58:29 +0100

    add: test to mema_text_to_speech.py

--- Codebank/mema_text_to_speech.py
+++ Codebank/mema_text_to_speech.py
@@ -12,7 +12,7 @@ def speak_thread(text) -> None:
     # Generating a pseudorandom file name using the current time in milliseconds
     # Since we need to save the file to play it, this can prevent duplicate file name errors.
     # If on the odd chance that this fails, it will be caught by the try-except
-    filename: str = str(time() * 100000).replace('.','s') + ".mp3"
+    filename: str = str(time.time() * 100000).replace('.','s') + ".mp3"
 
     # Save the file, play is using the playsound library then remove it.
     try:
@@ -34,5 +34,5 @@ def speak(text) -> None:
 if __name__ == "__main__":
     speak("Test 1")
     time.sleep(1)
-    speak("Test 2")
-    speak("Test 3")
+    speak("Concurrent TTS")
+    speak("TTS Concurrently")
