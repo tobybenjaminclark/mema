6d0a6b8862ed4348fc6138b6afc3b84b1776b0a7
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-07-03 15:15:54 +0100

    add: working take image

--- codebank/mema_content_new_user_photo.py
+++ codebank/mema_content_new_user_photo.py
@@ -5,29 +5,41 @@ from PIL import ImageTk, Image
 """
 camera = cv2.VideoCapture(0)
             return_value, image = camera.read()
-            cv2.imwrite('opencv.png', image)
-            del(camera)
+            
 """
 
 class content_new_user_photo(content_frame):
 
     def __init__(self, parent, name, *args, **kwargs):
         content_frame.__init__(self, *args, **kwargs)
+
+        self.current_image: Image = None
+
         self.parent = parent
         self.name = name
 
         self.label = Label(self, text = "LOGIN")
         self.label.grid(row=0, column=0)
-        self.label =Label(self)
+        self.label = Label(self)
         self.label.grid(row=0, column=0)
-        self.cap= cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(0)
         self.show_frames()
 
-    # Define function to show frame
+        self.update_buttons()
+
+    def update_buttons(self) -> None:
+
+        buttons: list[(str, str)] = [0, 0, 0, 0]
+        buttons[0] = ("Take Photo", "TAKE_PHOTO")
+        buttons[1] = (None, None)
+        buttons[2] = (None, None)
+        buttons[3] = (None, None)
+        self.parent.set_input(buttons, False)
+
     def show_frames(self):
         # Get the latest frame and convert into Image
-        cv2image= cv2.cvtColor(self.cap.read()[1],cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(cv2image)
+        self.current_image = cv2.cvtColor(self.cap.read()[1],cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(self.current_image)
         # Convert image to PhotoImage
         imgtk = ImageTk.PhotoImage(image = img)
         self.label.imgtk = imgtk
@@ -36,4 +48,7 @@ class content_new_user_photo(content_frame):
         self.label.after(20, self.show_frames)
 
     def callback(self, callback_request: dict[str:str]):
-        pass
+        
+        if(callback_request["content"] == "TAKE_PHOTO"):
+            cv2.imwrite('opencv.png', cv2.cvtColor(self.current_image, cv2.COLOR_BGR2RGB))
+            
