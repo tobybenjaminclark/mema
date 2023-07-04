171190d9b343991261af2fce74bdc7935cc47fd7
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-07-04 10:09:57 +0100

    add: docs to mema_text_to_speech.py

--- codebank/mema_text_to_speech.py
+++ codebank/mema_text_to_speech.py
@@ -6,6 +6,19 @@ from mema_constants import *
 import time
  
 def speak_thread(text: str) -> None:
+    """
+    Generates speech from the given text using Google Text-to-Speech (gTTS) and plays it using the playsound library.
+    The gTTS library requires an internet connection to convert text to speech, an appropriate audioplayer is required
+    to play sound aloud using the playsound library. This shouldn't be called directly, instead use the speak() command.
+
+    Args:
+        text (str): The input text to be converted into speech.
+
+    Example:
+        >>> speak_thread("Hello, how are you?")
+        Speech generated from the text "Hello, how are you?" is played.
+
+    """
 
     # Create a Google TTS Object
     tts: gTTS = gTTS(text=text, lang='en', slow = MEMA_SLOW_TTS)
@@ -27,7 +40,11 @@ def speak_thread(text: str) -> None:
     return None
 
 def speak(text: str) -> None:
-        
+    """
+    Converts the passed text to speech and plays it asynchronously on a thread using the speak_thread() function.
+    Handles error checking for an empty string. Uses the gTTS library, requires an Internet Connection (currently)
+    """
+
     if(text == ""):
         return None
 
