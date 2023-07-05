c3354c9aa36c35f8198f6cce869acaed98dd633a
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-07-05 11:30:16 +0100

    add: full refactor of mema_content_login.py

--- codebank/mema_content_login.py
+++ codebank/mema_content_login.py
@@ -10,49 +10,56 @@ class content_login(content_frame):
     def __init__(self, parent, *args, **kwargs)-> None:
         """
         Initiazation method calls the superclass constructor, before setting up the buttons and creating the
-        Facial Recognition Frame. See mema_facial_recognition for how that works.
+        GUI using the setup_gui() method.
         """
         content_frame.__init__(self, *args, **kwargs)
 
-        # Sets the parent, updates the buttons
+        # Sets the parent, updates the buttons and initializes the GUI
         self.parent = parent
         self.update_buttons()
-
-        # Sets the current face to Unknown.
-        self.current_face = "Unknown"
-
         self.setup_gui()
     
-    def setup_title(self) -> None:
-        # This will display MeMa at the top of the screen
-        title: str = "Memory Machine"
-        self.mema_label: Label
+    def setup_title(self, title: str = "Memory Machine") -> None:
+        """
+        Creates a title label and packs it to the screen. 
+        """
 
+        self.mema_label: Label
         self.mema_label = Label(self, text = title, font = ("Arial", 56, "bold"), bg = MEMA_WHITE, fg = MEMA_BLACK)
         self.mema_label.pack(pady = 20)
 
     def setup_facial_recognition_frame(self) -> None:
-        # This sets up the Facial Recognition Frame, which recognizes faces using cv2 & face_recognition modules.
-        # It will display the webcam, and will pass the recognized face UserID to facial_recognition_callback() method.
+        """
+        This sets up the Facial Recognition Frame, which recognizes faces using cv2 & face_recognition modules.
+        It will display the webcam, and will pass the recognized face UserID to facial_recognition_callback() method.
+        """
+        self.current_face: str = "Unknown"
         self.facial_recognition_frame: facial_recognition
         self.facial_recognition_frame = facial_recognition(self, self.facial_recognition_callback, width = 800)
         self.facial_recognition_frame.pack()
 
-    def setup_description(self) -> None:
-        # This will display text under the facial recognition panel
-        description:str = "Look at the Camera and say 'LOGIN'"
+    def setup_description(self, description: str = "Look at the Camera & Say 'LOGIN'") -> None:
+        """
+        Sets up the description label that instructs the user on how to login.
+        """
+
         self.mema_description: Label
         self.mema_description = Label(self, text = description, font = ("Arial", 35, "bold"), bg = MEMA_WHITE, fg = MEMA_BLACK)
         self.mema_description.pack(pady = (20,5))
 
-    def setup_subdescription(self) -> None:
-        # Displays sub-description label
-        sub_description:str = "New to Memory Machine? Say 'NEW'"
+    def setup_subdescription(self, sub_description: str = "New to Memory Machine? Say 'NEW'") -> None:
+        """
+        Sets up the sub-description label which tells the user how to create a new account
+        """
+
         self.mema_sub_description: Label
         self.mema_sub_description = Label(self, text = sub_description, font = ("Arial", 30), bg = MEMA_WHITE, fg = MEMA_BLACK)
         self.mema_sub_description.pack(pady = (0,20))
 
     def setup_gui(self) -> None:
+        """
+        Initializes the GUI for this frame by calling the appropriate methods in the correct order.
+        """
 
         self.setup_title()
         self.setup_facial_recognition_frame()
@@ -76,9 +83,14 @@ class content_login(content_frame):
         """
         Updates the currently recognized face, this is used if the user activates the LOGIN flow.
         """
+
         self.current_face = name
 
     def __del__(self) -> None:
+        """
+        On deletion, tells the facial_recognition frame to quit
+        """
+
         self.facial_recognition_frame.quit()
 
     def callback(self, callback_request:dict[str:str]) -> None:
