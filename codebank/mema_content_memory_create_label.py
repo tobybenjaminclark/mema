30fab30f7ada25cfff4a95bc1d1e39c62ee6eaca
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-07-07 11:56:21 +0100

    add: file for label creation using speech recognition

---
+++ codebank/mema_content_memory_create_label.py
@@ -0,0 +1,9 @@
+from mema_content_frame import *
+
+class content_memory_create_label(content_frame):
+
+    def __init__(self, memoryspace_path: str) -> None:
+        pass
+
+    def callback(self, callback_request: dict) -> None:
+        pass
