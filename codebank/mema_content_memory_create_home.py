c207a5bfb7d8c9af44880ed52bb38c3cedddc74f
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-07-11 14:40:00 +0100

    add: remove random audio file from repo

--- codebank/mema_content_memory_create_home.py
+++ codebank/mema_content_memory_create_home.py
@@ -76,7 +76,7 @@ class content_memory_create_home(content_frame):
         # Finds the max frame. (Adds 1, as always needs an empty frame to add to)
         # Error checking
         try:
-            self.max_frames = int(max([file.path.rpartition("/")[2][0] for file in os.scandir(memoryspace_path)]))+2
+            self.max_frames = int(max([file.path.rpartition("/")[2][0] for file in os.scandir(self.memoryspace_path)]))+2
         except:
             self.max_frames = 1
         finally:
